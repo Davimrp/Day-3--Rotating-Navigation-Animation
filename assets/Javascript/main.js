7b2a601b85744c03fe7001f5ea15e46290b9d0ac
Davimrp <daviwendell2015@gmail.com>
2023-03-17 13:39:55 -0300

    Adicionando animações no menu

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -1,16 +1,22 @@
-const menu = document.getElementById('menu');
+const menu = document.getElementById('circle-menu');
+const menuIcon = document.querySelector('menu');
 const pagina = document.querySelector('.container');
-const nav = document.querySelector('nav')
+const nav = document.querySelector('nav');
+const i = document.querySelector('i')
 
 function abreFecha(){
     if(!pagina.classList.contains('menu-active')){
         pagina.classList.add('menu-active');
         nav.classList.remove('some-menu');
         nav.classList.add('aparece-menu');
+        menu.style.transform = 'rotate(-65deg)';
+        i.classList.add('fa-times');
     }else{
         pagina.classList.remove('menu-active');
         nav.classList.remove('aparece-menu');
         nav.classList.add('some-menu');
+        menu.style.transform = 'rotate(-1deg)';
+
     }
 }
-menu.addEventListener('click', abreFecha)
+menu.addEventListener('click', abreFecha);
