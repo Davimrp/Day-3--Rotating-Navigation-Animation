5253dae06eb788a05bde4130f2600bb40afdfc51
Davimrp <daviwendell2015@gmail.com>
2023-03-17 01:58:52 -0300

    logica js terminada

--- assets/Javascript/main.js
+++ assets/Javascript/main.js
@@ -5,12 +5,10 @@ const nav = document.querySelector('nav')
 function abreFecha(){
     if(!pagina.classList.contains('menu-active')){
         pagina.classList.add('menu-active');
-        document.querySelector('body').style.overflow = 'hidden';
         nav.classList.remove('some-menu');
         nav.classList.add('aparece-menu');
     }else{
         pagina.classList.remove('menu-active');
-        document.querySelector('body').style.overflow = 'auto';
         nav.classList.remove('aparece-menu');
         nav.classList.add('some-menu');
     }
